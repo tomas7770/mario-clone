774f92c68e1c2e5556699594da81ffdafb07383d
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-02-12 11:49:17 +0000

    Add player gravity

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+export (float) var gravity = 2500.0
+
+onready var body = $Body
+onready var sprite = $AnimatedSprite
+
+var velocity = Vector2()
 
 func _ready():
 	pass
+
+func _process(_delta):
+	sprite.transform = body.transform
+
+func _physics_process(delta):
+	velocity.y += gravity*delta
+	velocity = body.move_and_slide(velocity, Vector2(0, -1))
