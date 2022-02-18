cc582b8526e19528b2e1b476acf9e5415f5ce157
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-02-18 21:30:53 -0100

    Generalize level loading

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -1,13 +1,11 @@
 extends Control
 
-onready var level = $Level
-onready var game_hud = $GameHUD
+var game_hud_scene = preload("res://Scenes/GameHUD/GameHUD.tscn")
+var level
+var game_hud
 
 func _ready():
-	for child in level.get_children():
-		if child is Player:
-			game_hud.track_player(child)
-			break
+	load_level("TestLevel")
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -19,3 +17,14 @@ func toggle_pause():
 		game_hud.on_pause()
 	else:
 		game_hud.on_unpause()
+
+func load_level(level_name):
+	var new_level = load("res://Scenes/Levels/"+level_name+".tscn")
+	level = new_level.instance()
+	add_child(level)
+	game_hud = game_hud_scene.instance()
+	add_child(game_hud)
+	for child in level.get_children():
+		if child is Player:
+			game_hud.track_player(child)
+			break
